74ce4e745ccd5b96e4f403912971df342ef7bc55
coutureonline15-commits <coutureonline15@gmail.com>
2025-09-16 13:14:54 -0700

    Create products.js
    created framework for store.html

---
+++ products.js
@@ -0,0 +1,11 @@
+// products.js
+const products = [
+  {id:1, name:"Oled Nintendo Switch with White Controllers", image:"product1.jpg", price:225, description:"Includes Joy-Cons and charger."},
+  {id:2, name:"Dual Sense Edge Playstation Controller", image:"product2.jpg", price:150, description:"Customizable PS5 controller."},
+  {id:3, name:"Dewalt Combo Kit", image:"product3.jpg", price:150, description:"Drill + impact driver combo."},
+  {id:4, name:"NutriBullet", image:"product4.jpg", price:110, description:"Never opened, 900W."},
+  {id:5, name:"Rog Ally X", image:"product5.jpg", price:799.99, description:"Handheld gaming PC."},
+  {id:6, name:"Xbox Series S With Controller", image:"product6.jpg", price:275, description:"512GB with controller."},  
+  {id:7, name:"ASUS TUF Gaming Laptop", image:"product7.jpg", price:629, description:"15.6”, Ryzen 7, RTX GPU."},        
+  {id:8, name:"1991 Classic Major League Baseball Trivia Boardgame Series 2", image:"product8.jpg", price:50, description:"Vintage trivia board game."}
+];
